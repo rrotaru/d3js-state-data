b2307cb63f4c7d96563c40fa8d733da1210a9e66
Robert Rotaru <rrotaru@wesleyan.edu>
2015-11-05 16:23:20 -0500

    Added colors for heatmap

--- js/state-data.js
+++ js/state-data.js
@@ -1,9 +1,11 @@
 var jsondata = {};
 var map;
 $.getJSON('json/stateaverages.json', function(dataset) {
+    console.log(dataset);
     var data = dataset.data;
     var columns = dataset.meta.view.columns;
     var nameField = 8; // Index of the name field in the state data object
+    var colorkey = ['lowest', 'very low', 'low', 'medium low', 'medium', 'medium high', 'high', 'very high', 'highest']
     
     // Format json data into the way we want it
     for (var i = 0; i < data.length; i++) {
@@ -31,8 +33,7 @@ $.getJSON('json/stateaverages.json', function(dataset) {
             if (min > val) min = val;
         }
         var diff = max - min;
-        var step = diff / 9.0; // 5 colors for the heatmap
-        //console.log(columns[i], min, max);
+        var step = diff / 8.0; // 5 colors for the heatmap
         j = 0;
         
         for (k = 0; k < data.length; k++) {
@@ -40,22 +41,28 @@ $.getJSON('json/stateaverages.json', function(dataset) {
             var fieldName = columns[i].fieldName;
             var fieldType = columns[i].dataTypeName;
             if (fieldType !== "meta_data" && fieldType !== "text" && fieldType !== 'calendar_date') {           
-                //console.log(i, k, stateName, fieldName, stepval);
 
-                var stepval = Math.ceil(parseInt(data[k][i]) / step);
-                jsondata[stateName][fieldName].fillKey = stepval;
+                var stepval = Math.ceil(parseInt((data[k][i] - min)*1000 )/ (step*1000));
+                jsondata[stateName][fieldName].fillKey = colorkey[stepval];
+                if (i === 9) {
+                    jsondata[stateName].fillKey = colorkey[stepval];
+                }
             }
         }
     }
     
+    function updateColors() {
+        for (var key in jsondata) {
+                jsondata[key].fillKey = jsondata[key][this.value].fillKey;
+            }
+            map.updateChoropleth(jsondata);
+    };
+
     var sel = $('<select>').appendTo('#controller').css({'float':'right'});
     var desc = $('<div>').appendTo('#controller').css({'position':'absolute','bottom':'8px'});
     desc.append($('<h1>').text(dataset.meta.view.name).css({'font-family':'sans-serif'}));
     desc.append($('<p>').text(dataset.meta.view.description).css({'font-family':'sans-serif','font-size':'14px'}));
-    sel.change(function(e) { 
-        console.log(this.value); 
-        map
-    });
+    sel.change(updateColors);
 
     for (var key in jsondata['NATION']) {
         sel.append($("<option>").attr('value',key).text(jsondata['NATION'][key].name));
@@ -65,15 +72,15 @@ $.getJSON('json/stateaverages.json', function(dataset) {
         scope: 'usa',
         element: document.getElementById('container'),
         fills: {
-            0 : 'hsl(240, 100%, 50%)',
-            1 : 'hsl(210, 100%, 50%)',
-            2 : 'hsl(180, 100%, 50%)',
-            3 : 'hsl(150, 100%, 50%)',
-            4 : 'hsl(120, 100%, 50%)',
-            5 : 'hsl(90, 100%, 50%)',
-            6 : 'hsl(60, 100%, 50%)',
-            7 : 'hsl(30, 100%, 50%)',                
-            8 : 'hsl(0, 100%, 50%)',
+            'lowest' : 'hsl(240, 100%, 50%)',
+            'very low' : 'hsl(210, 100%, 50%)',
+            'low' : 'hsl(180, 100%, 50%)',
+            'medium low' : 'hsl(150, 100%, 50%)',
+            'medium' : 'hsl(120, 100%, 50%)',
+            'medium high' : 'hsl(90, 100%, 50%)',
+            'high' : 'hsl(60, 100%, 50%)',
+            'very high' : 'hsl(30, 100%, 50%)',                
+            'highest' : 'hsl(0, 100%, 50%)',
             defaultFill: '#999'
         },
         geographyConfig: {
